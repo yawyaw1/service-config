87ad08b1b1664b6928964a74975633beabe66383
youssef smimaa <youssef.smimaa@gmail.com>
2019-02-13 10:53:44 +0100

    kick off

---
+++ src/main/java/fr/perioline/server/config/PeriolineServerConfigApplication.java
@@ -0,0 +1,17 @@
+package fr.perioline.server.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+
+@EnableConfigServer
+@SpringBootApplication
+public class PeriolineServerConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeriolineServerConfigApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/fr/perioline/server/config/PeriolineServerConfigApplicationTests.java
@@ -0,0 +1,17 @@
+package fr.perioline.server.config;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PeriolineServerConfigApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
