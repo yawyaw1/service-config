02fb4fa0351cb478d2c1a4456b2cca1118553b86
youssef smimaa <youssef.smimaa@gmail.com>
2019-05-25 13:04:33 +0200

    [service-config] update classes name

--- src/main/java/com/config/service/ConfigServiceApplication.java
+++ src/main/java/com/config/service/ConfigServiceApplication.java
@@ -1,4 +1,4 @@
-package fr.perioline.config.service;
+package com.config.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,9 +7,9 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
-public class PeriolineConfigServiceApplication {
+public class ConfigServiceApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(PeriolineConfigServiceApplication.class, args);
+		SpringApplication.run(ConfigServiceApplication.class, args);
 	}
 
 }
